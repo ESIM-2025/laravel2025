64af5011dadf64d04569b4929d7e2671d9ce1c83
JampOreo <joaquinmedinapedrozo@gmail.com>
2025-08-20 10:25:20 -0300

    agregado partial presentacion joaquin medina

---
+++ resources/views/partials/joaquin.blade.php
@@ -0,0 +1,6 @@
+<div class="presentacion">
+    <h2>holala</h2>
+    <img src="https://i.makeagif.com/media/8-03-2015/fAkfGz.gif" width="500" height="300">
+    <p>Mi nombre es Joaquin, me gusta la robotica, la programación, River Plate y jugar jueguitos.</p>
+    <a href="https://www.about.me/joaquinmedina" target="_blank">Para saber mas sobre mí, podes visitar esta pagina web.</a>
+</div>

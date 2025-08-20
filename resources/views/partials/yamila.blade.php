cc74ba2035fff60557f95e1b6914d18113bdebb5
Yamila Almada <yamila.almada@esim.edu.ar>
2025-08-20 03:52:27 -1000

    Agregado partial de presentación Yamila

---
+++ resources/views/partials/yamila.blade.php
@@ -0,0 +1,10 @@
+<div class="presentacion">
+    <h2>¡Hola! Soy Almada Yamila</h2>
+    <p>Estudiante de 5to Año A </p>
+    <ul>
+        <li><strong>Soy Yamila Almada, Tengo 17 años, Vivo en Posadas.
+            Me gusta el Taekwon-Do, me gusta las juntadas de motos, me gusta la rpogramacion a pesar de que aveces se me dificulta, es algo que me llama mucho 
+            la atencion y me gusta. Me gusta pasar tiempo con mi familia y pareja, tambien hacer ejercicio, salir a caminar y andar en bici.
+     </li>
+    </ul>
+</div>

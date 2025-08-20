331b8bf0c9751ba9d0abf719f4dd471e9ea7f168
brisa <brisamaylenromeronunez@gmail.com>
2025-08-20 10:55:59 -0300

    Rama de ana Rindflisch

---
+++ resources/views/partials/Ana.blade.php
@@ -0,0 +1,6 @@
+<div class="presentacion">
+    <h2>Hola, soy Ana Rindfleisch</h2>
+    <p>Hols, soy estudiante de 5to año, me gusta el arte, el diseño web, la decoracion, las artes el general
+        voy a estudiar psicologia en Rosario Santa fe, y amo a mi perro Principe
+    </p>
+</div>

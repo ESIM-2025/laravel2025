5d817a4720a6fe3dfebdd2be0382e0763a3f333d
SusaEB <susana.bergalo@esim.edu.ar>
2025-08-20 09:59:15 -0400

    Presentacion de Mati

---
+++ resources/views/partials/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Presentacion Matias</title>
+</head>
+<body>
+    <h1>Quien soy?</h1>
+    <p>Hola, soy Matias Rodriguez. Estudio en la Escuela Secundaria de Innovacion de Misiones, tambien, estudie ingles en Imes</p>
+    <br>
+    <p>Disfruto jugar deportes como Futbol, con amigos mas que nada. Me gusta los jueguitos de play y juntarme con amigos</p>
+    <img src="img/matifoto.jpeg" alt="">
+</body>
+</html>

a64607206fe06470df6de8450d59d4383f0d972d
catalevyyy <peralta.catalina@esim.edu.ar>
2025-08-20 10:54:45 -0300

    Cata

---
+++ resources/views/Cata/Cata.blade.php
@@ -0,0 +1,4 @@
+<div class="presentacion">
+    <h2>CATALINA LEVY</h2>
+    <p><strong>SOBRE MI:</strong> Hola soy Cata Levy, tengo 17 años, me gusta jugar al fútbol, pintar, dibujar y jugar a la compu. Amo a mis amiguitos, y a Cande y Emi en especial.</p>
+</div>

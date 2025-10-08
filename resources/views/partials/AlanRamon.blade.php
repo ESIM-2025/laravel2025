d0e7ff01003bc44d3c109be487a6b8d01fc53de9
alanramon-ui <alan.ramon@esim.edu.ar>
2025-10-08 07:44:20 -0600

    Agrega partial de presentación personal: AlanRamon

---
+++ resources/views/partials/AlanRamon.blade.php
@@ -0,0 +1,9 @@
+<!-- resources/views/partials/maria-garcia.blade.php -->
+<div class="card mt-4">
+    <div class="card-header bg-primary text-white">
+        <h3>Mi Presentación</h3>
+    </div>
+    <div class="card-body">
+        <p><strong>Nombre:</strong> Alan Ramon. me gusta dibujar</p>
+    </div>
+</div>

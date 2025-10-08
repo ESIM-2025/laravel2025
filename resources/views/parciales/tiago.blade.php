1cd9e8ad5fcadb3f86e370e182241510493bd774
Tiago Espinola <tiago.espinola@esim.edu.ar>
2025-10-08 10:28:15 -0300

    Mi presentacion

---
+++ resources/views/parciales/tiago.blade.php
@@ -0,0 +1,5 @@
+<div class="card">
+    <h2>¡Hola! Soy Tiago Espinola</h2>
+    <p>Estudiante de la escuela secundaria innovacion de misiones, que le costo un poco el laravel.</p>
+    <p>Me gusta los juegos. </p>
+</div>

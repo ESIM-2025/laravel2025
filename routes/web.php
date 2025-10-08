a42de2a0ba3fee36ccfb45ced158c88dc4d8b5b3
tareasnico <perez.nicolas@esim.edu.ar>
2025-10-08 12:00:35 -0300

    Trabajo terminado: ModeloControlador, Producto.php, modeol.blade.php y modificación del web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ModelController;
+
+
 
 Route::get('/', function () {
     return view('partials.ludmila'); // usa el punto para indicar subcarpeta
 });
+Route::get('/', [ModelController::class, 'index']);
 

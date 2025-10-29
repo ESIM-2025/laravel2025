41986f4fe1a97b1d553dd3ff6e7af900816c51c3
anitawiwiw <lunaaviri27@gmail.com>
2025-10-29 11:02:44 -0300

    feat: add tutorial básico modificado a las 11:02

--- resources/views/Tutorial/controller.blade.php
+++ resources/views/Tutorial/controller.blade.php
@@ -13,13 +13,43 @@
     <h2>Comandos Básicos:</h2>
     <pre>
 // Crear controller básico
+        <h3>Como ya habiamos clonado todo, lo unico q nos queda por hacer es esto: git checkout -b feature-controller
+        eso va en la terminal, despues  vamos a crear el controlador. 
 php artisan make:controller MiController
 
-// Crear controller con CRUD completo
-php artisan make:controller ProductController --resource
-
-// Crear controller para API
-php artisan make:controller UserController --api
-    </pre>
+        Una ves queya creamos vamos a resources/views/tutorial-controller.blade.php
+        Ahí primero creamos en viws una carpeta llamda tutorial, despues dentro de tutorial hacemos un blade : controller.blade.php
+        Una ves que ya tenemos eso creado, dentro del blade vamos a poner lo siguiente (como base)<h3>
+            
+        (<!DOCTYPE html>
+<html>
+<head>
+    <title>Tutorial - Controllers</title>
+</head>
+<body>
+    <h1>🚀 Tutorial de Controllers</h1>
+    <p>Controlador creado exitosamente</p>
 </body>
-</html>
+</html>)
+        ()<-- los parentesis son para marcar lo q tenemos q poner nomas.
+        Despues de que se creo nuestro tutorialcontroller, vamos a agregarle el siguiente codigo
+(<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class TutorialController extends Controller
+{
+    public function index()
+    {
+        return view('tutorial-controller');
+    }
+})
+
+Una ves que ya tenemos eso vamosa  routes/web.php y al codigo que ya tenemos ahí vamos agregar al final sin modificar nada el siguiente codigo
+    (use App\Http\Controllers\TutorialController;
+Route::get('/tutorial-controller', [TutorialController::class, 'index']);)
+
+Y por ultimo tenemos q subir todo al github, 
+    (git add .
+git commit -m "feat: add tutorial básico sobre controllers"
+git push origin feature-controller)
